6466994541d9cd32d82c9caeaf38df95a3ef05a9
mhrjnrahul <maharjanrahul58@gmail.com>
2025-06-26 13:44:11 +0545

    hero bg adjustments

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='w-full h-full flex flex-col justify-center items-center'>
+    <div className='h-screen w-full'>
+        <div className='bg-[url("/thumbnail.png")] bg-no-repeat bg-cover w-full h-full flex flex-col justify-start items-center '>
         <div className='flex flex-col mt-10'>
             <p className='text-4xl text-center'>Publish your passions, your way</p>
             <p className='text-2xl text-center mt-3'>Create a unique and beautiful blog easily.</p>
         </div>
         <button className='bg-black text-xl text-white p-5 rounded-2xl mt-15'>CREATE YOUR BLOG</button>
+    </div>
     </div>
   )
 }
