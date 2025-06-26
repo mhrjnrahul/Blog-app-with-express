5641133ee8644ea39d59511c672eb863eea494d3
mhrjnrahul <maharjanrahul58@gmail.com>
2025-06-26 13:50:09 +0545

    adjustments on hero and nav

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div className='h-screen w-full'>
-        <div className='bg-[url("/thumbnail.png")] bg-no-repeat bg-cover w-full h-full flex flex-col justify-start items-center '>
-        <div className='flex flex-col mt-10'>
-            <p className='text-4xl text-center'>Publish your passions, your way</p>
-            <p className='text-2xl text-center mt-3'>Create a unique and beautiful blog easily.</p>
+    <div className="h-screen w-full">
+      <div className='bg-[url("/thumbnail.png")] bg-no-repeat bg-cover w-full h-full flex flex-col justify-start items-center '>
+        <div className="flex flex-col mt-10">
+          <p className="text-4xl text-center">
+            Publish your passions, your way
+          </p>
+          <p className="text-2xl text-center mt-3">
+            Create a unique and beautiful blog easily.
+          </p>
         </div>
-        <button className='bg-black text-xl text-white p-5 rounded-2xl mt-15'>CREATE YOUR BLOG</button>
+        <div className="flex flex-row gap-5">
+          <button className="bg-black text-xl text-white p-5 rounded-2xl mt-15">
+            CREATE YOUR BLOG
+          </button>
+          <button className="bg-black text-xl text-white p-5 rounded-2xl mt-15">
+            SEE ALL BLOGS
+          </button>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between items-center bg-black text-white px-10 w-full h-16'>
-      <div>Blogger</div>
+    <nav className='bg-[url("/thumbnail.png")] bg-no-repeat bg-cover flex justify-between items-center text-white px-10 w-full h-16'>
+      <div className='text-2xl'>Blogger</div>
       <div>
-        <button>Signup</button>
+        <button>SIGN IN</button>
       </div>
     </nav>
   )
