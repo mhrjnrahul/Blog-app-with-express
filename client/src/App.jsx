801e8bb837462d961f6befebdd857b810f831b30
mhrjnrahul <maharjanrahul58@gmail.com>
2025-06-26 15:26:34 +0545

    added another component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Extra from './components/Extra'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Extra />
     </div>
   )
 }
---
+++ client/src/components/Extra.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Extra = () => {
+  return (
+    <div className='w-full h-screen bg-[url("/perfect.png")] bg-no-repeat bg-cover flex items-center justify-end'>
+        <div className='flex flex-col w-1/2 max-w-md p-8 mr-8'>
+            <p className='text-2xl font-bold mb-4'>Choose the perfect design</p>
+            <p className='text-lg leading-relaxed'>Create a beautiful blog that fits your style. Choose from a selection of easy-to-use 
+                templates – all with flexible layouts and hundreds of background images – or design 
+                something new.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Extra
